021c338bede72ec080cdbf36f60258df81b2fdd9
Esteban Zambrano Garoz <zam22119@uvg.edu.gt>
2026-03-12 19:22:24 -0600

    feat: generate public and private keys

--- generar_claves.py
+++ generar_claves.py
@@ -1,9 +1,29 @@
 from Crypto.PublicKey import RSA
 
 def generar_par_claves(bits: int = 3072):
-    # TODO: implementar con RSA.generate()
-    # Recordar: mínimo 2048 bits; recomendado 3072 para nuevo software
-    pass
+
+    if bits < 2048:
+        raise ValueError("RSA debe usar al menos 2048 bits")
+
+    key = RSA.generate(bits)
+
+    public_key = key.publickey()
+
+    private_pem = key.export_key(
+        format="PEM",
+        passphrase="lab04uvg",
+        pkcs=8,
+        protection="PBKDF2WithHMAC-SHA512AndAES256-CBC"
+    )
+
+    public_pem = public_key.export_key(format="PEM")
+
+    with open("private_key.pem", "wb") as f:
+        f.write(private_pem)
+
+    with open("public_key.pem", "wb") as f:
+        f.write(public_pem)
+    
 
 if __name__ == '__main__':
     generar_par_claves(3072)
